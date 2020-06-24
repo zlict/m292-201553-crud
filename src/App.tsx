b6cc6a871d1cd7d2db8c0353c5d79336d6bf9fa2
Robin Bühler <public@openscript.ch>
2020-06-24 15:48:12 +0200

    Add simple context api example

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { UsersContainer } from './containers/Users';
 import { HelloContainer } from './containers/Hello';
 import { UppercaseContainer } from './containers/Uppercase';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path='/users'>
-            <UsersContainer />
-          </Route>
-          <Route path='/hallo/:name'>
-            <HelloContainer />
-          </Route>
-          <Route path='/uppercase'>
-            <UppercaseContainer />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+export const CounterContext = createContext<[number, React.Dispatch<number>]>([0, (newCounter: number) => {}]);
+
+export const PlusOne = () => {
+  const [counter, setCounter] = useContext(CounterContext);
+  
+  return (<button onClick={() => setCounter(counter + 1) }>+1</button>);
 }
 
-export default App;
+export const App = () => {
+  const counterState = useState(0);
+
+  return (
+    <CounterContext.Provider value={counterState}>
+      <BrowserRouter>
+        <PlusOne />
+        <div className="App">
+          <Switch>
+            <Route path='/users'>
+              <UsersContainer />
+            </Route>
+            <Route path='/hallo/:name'>
+              <HelloContainer />
+            </Route>
+            <Route path='/uppercase'>
+              <UppercaseContainer />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </CounterContext.Provider>
+  );
+}
--- src/containers/Hello.tsx
+++ src/containers/Hello.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from "react-router-dom";
+import { CounterContext } from '../App';
 
 type RouteParams = {
     name: string;
@@ -7,9 +8,11 @@ type RouteParams = {
 
 export const HelloContainer: React.FC = () => {
   const params = useParams<RouteParams>();  
+  const value = useContext(CounterContext);
   return (
     <>
-      Hallo {params.name}
+      Hallo {params.name}!
+      Counter: {value}
       <Link to="/users">Zu den Benutzern</Link>
     </>
   );
